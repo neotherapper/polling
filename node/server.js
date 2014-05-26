6a628c2ef46128cebaba1678b11bdc4cd7b0369c
neotherapper <george.pilitsoglou@gmail.com>
2014-05-26 05:27:19 +0200

    removed ,

--- node/server.js
+++ node/server.js
@@ -37,7 +37,7 @@ var app = http.createServer(function(request, response) {
 }),
 // io = require('socket.io',).listen(app),
 // fixed http://stackoverflow.com/questions/8350630/nodejs-with-socket-io-delay-emitting-data
-io = require('socket.io', { rememberTransport: false, transports: ['WebSocket', 'Flash Socket', 'AJAX long-polling'] }).listen(app),
+io = require('socket.io', { rememberTransport: false, transports: ['WebSocket', 'Flash Socket', 'AJAX long-polling'] }).listen(app);
 fs = require('fs');
 
 app.listen(5000);
